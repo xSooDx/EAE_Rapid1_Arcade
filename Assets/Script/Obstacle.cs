45e65f164efeb375fbdaa933543d5122a50c855f
Saurabh Sood <u1423247@umail.utah.edu>
2022-09-08 19:54:34 -0600

    Obstacles + Bg + Particles

--- Assets/Script/Obstacle.cs
+++ Assets/Script/Obstacle.cs
@@ -6,7 +6,12 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     [SerializeField] Rigidbody2D myRigidbody;
+    [SerializeField] SpriteRenderer mySprite;
+    [SerializeField] ParticleSystem explosionParticles;
+    [SerializeField] ParticleSystem trailParticles;
     public string description = "Knocked Out!";
+    public float explosionRadius = 1.5f;
+    public float explosionForce = 4f;
 
     public Vector2 velocity
     {
@@ -31,5 +36,20 @@ public class Obstacle : MonoBehaviour
         {
             if (GameEventManager.gameEvent != null) GameEventManager.gameEvent.GameOver.Invoke("Ship Crashed!!", description, false, true);
         }
+
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+        
+        foreach (Collider2D col in hits)
+        {
+            if (!col || !col.attachedRigidbody) continue;
+            Vector2 dir = col.attachedRigidbody.position - (Vector2)transform.position;
+            dir = (dir.normalized + -myRigidbody.velocity.normalized * 0.25f).normalized;
+            col.attachedRigidbody.AddForce(dir * explosionForce, ForceMode2D.Impulse);
+        }
+        // ToDo Explosion Prefab
+        ParticleSystem explodeParticles = Instantiate(explosionParticles, transform.position, transform.rotation);
+        ParticleSystem.MainModule main = explodeParticles.main;
+        main.startColor = trailParticles.main.startColor;
+        Destroy(this.gameObject);
     }
 }
--- Assets/Script/PlanetObstacleSpawner.cs
+++ Assets/Script/PlanetObstacleSpawner.cs
@@ -6,46 +6,74 @@ public class PlanetObstacleSpawner : MonoBehaviour
 {
     public PointEffector2D gravitySource;
 
-    public Obstacle obstaclePrefab;
+    public Obstacle[] obstaclePrefabs;
 
     public int obstacleCount;
     public float minHeight;
     public float maxHeight;
+    public float obstacleVelocity = 2f;
+    public float timeBetweenSpawns = 3.5f;
+    public float randomVelAngle = 15f;
 
 
     private void Start()
     {
-        SpawnObstacle();
+        StartCoroutine(SpawnObstacleRoutine());
     }
 
 
-    Vector2 CalculateOrbitalVelocity(Vector2 position, float mass)
-    {
+    //Vector2 CalculateOrbitalVelocity(Vector2 position, float mass)
+    //{
 
-        float orbitalSpeed = Mathf.Sqrt(Mathf.Abs(gravitySource.forceMagnitude) * ((Vector2)transform.position - position).magnitude / mass);
-        //Debug.Log($"float {orbitalSpeed} = Mathf.Sqrt({gravitySource.forceMagnitude} * {((Vector2)transform.position - position).magnitude} / {mass})");
-        Vector2 delta = ((Vector2)transform.position - position).normalized;
-        Vector2 tangent = Quaternion.Euler(0, 0, 90) * delta;
-        return tangent * orbitalSpeed;
+    //    float orbitalSpeed = Mathf.Sqrt(Mathf.Abs(gravitySource.forceMagnitude) * ((Vector2)transform.position - position).magnitude / mass);
+    //    //Debug.Log($"float {orbitalSpeed} = Mathf.Sqrt({gravitySource.forceMagnitude} * {((Vector2)transform.position - position).magnitude} / {mass})");
+    //    Vector2 delta = ((Vector2)transform.position - position).normalized;
+    //    Vector2 tangent = Quaternion.Euler(0, 0, 90) * delta;
+    //    return tangent * orbitalSpeed;
+    //}
+
+    //void SpawnObstacles()
+    //{
+    //    float angleBetweenSpawns = 360f / obstacleCount;
+    //    for (int i = 1; i <= obstacleCount; i++)
+    //    {
+    //        float height = Random.Range(minHeight, maxHeight);
+    //        Quaternion spawnAngle = Quaternion.Euler(0, 0, angleBetweenSpawns * i);
+    //        Vector3 spawnPos = transform.position + spawnAngle * new Vector2(0, height * transform.localScale.y);
+
+    //        Obstacle obs = Instantiate(obstaclePrefab, spawnPos, spawnAngle);
+
+    //        int direction = Random.Range(0, 2) == 1 ? -1 : 1;
+    //        //obs.velocity = direction * CalculateOrbitalVelocity(spawnPos, obs.mass);
+    //        obs.velocity = (transform.position - spawnPos).normalized * obstacleVelocity;
+    //        obs.angularVelocity = direction * Random.Range(0f, 30f);
+    //    }
+    //}
+
+    void SpawnObstacles(float angle)
+    {
+        float height = Random.Range(minHeight, maxHeight);
+        Quaternion spawnAngle = Quaternion.Euler(0, 0, angle);
+        Vector3 spawnPos = transform.position + spawnAngle * new Vector2(0, height * transform.localScale.y);
+
+        Obstacle obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+
+        Obstacle obs = Instantiate(obstaclePrefab, spawnPos, spawnAngle);
+
+        int direction = Random.Range(0, 2) == 1 ? -1 : 1;
+        //obs.velocity = direction * CalculateOrbitalVelocity(spawnPos, obs.mass);
+        obs.velocity = Quaternion.Euler(0,0,Random.Range(-randomVelAngle, randomVelAngle)) * (transform.position - spawnPos).normalized * obstacleVelocity;
+        obs.angularVelocity = direction * Random.Range(0f, 30f);
     }
 
-    void SpawnObstacle()
+
+    IEnumerator SpawnObstacleRoutine()
     {
-        float angleBetweenSpawns = 360f / obstacleCount;
-        for (int i = 1; i <= obstacleCount; i++)
+        while (true)
         {
-            float height = Random.Range(minHeight, maxHeight);
-            Quaternion spawnAngle = Quaternion.Euler(0, 0, angleBetweenSpawns * i);
-            Vector3 spawnPos = transform.position + spawnAngle * new Vector2(0, height * transform.localScale.y);
-
-            Obstacle obs = Instantiate(obstaclePrefab, spawnPos, spawnAngle);
-
-            int direction = Random.Range(0, 2) == 1 ? -1 : 1;
-            obs.velocity = direction * CalculateOrbitalVelocity(spawnPos, obs.mass);
-            obs.angularVelocity = direction * Random.Range(0f, 30f);
+            yield return new WaitForSeconds(timeBetweenSpawns);
+            SpawnObstacles(Random.Range(0, 360f));
         }
-
-        Instantiate(obstaclePrefab);
     }
 
 #if UNITY_EDITOR
